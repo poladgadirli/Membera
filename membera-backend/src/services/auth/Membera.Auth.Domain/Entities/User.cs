3ec70b795c362b23e20bbcc4157f1cd08af82702
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-09-03 23:05:43 +0400

    Add User and RefreshToken entities

---
+++ membera-backend/src/services/auth/Membera.Auth.Domain/Entities/User.cs
@@ -0,0 +1,38 @@
+﻿namespace Membera.Auth.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Email { get; private set; }
+    public string PasswordHash { get; private set; }
+    public bool IsEmailVerified { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    private User() { } 
+
+    public User(string firstName, string lastName, string email, string passwordHash)
+    {
+        Id = Guid.NewGuid();
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        PasswordHash = passwordHash;
+        IsEmailVerified = false;
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public void MarkEmailAsVerified()
+    {
+        IsEmailVerified = true;
+    }
+
+    public void UpdateProfile(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public string FullName => $"{FirstName} {LastName}";
+}
--- membera-backend/src/services/auth/Membera.Auth.Domain/obj/Debug/net8.0/Membera.Auth.Domain.AssemblyInfo.cs
+++ membera-backend/src/services/auth/Membera.Auth.Domain/obj/Debug/net8.0/Membera.Auth.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Membera.Auth.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04137277adb8fb8c2fe89b55bedd98a4f64151f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Membera.Auth.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Membera.Auth.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
