95e5205e0ae60b46ec03c87c1fd582aa55d0b128
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-09-04 15:52:19 +0400

    AuthDbContext

--- membera-backend/src/services/auth/Membera.Auth.Infrastructure/Persistence/AuthDbContext.cs
+++ membera-backend/src/services/auth/Membera.Auth.Infrastructure/Persistence/AuthDbContext.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Membera.Auth.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
-namespace Membera.Auth.Infrastructure.Persistence
+namespace Membera.Auth.Infrastructure.Persistence;
+
+public class AuthDbContext : DbContext
 {
-    internal class AuthDbContext
+    public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
     {
+    }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AuthDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
     }
 }
