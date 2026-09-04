b977d9f11f73f15bffdde1318015d170f3e46628
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-09-04 17:28:57 +0400

    LoginHandler

---
+++ membera-backend/src/services/auth/Membera.Auth.Application/Abstractions/IRefreshTokenRepository.cs
@@ -0,0 +1,10 @@
+﻿using Membera.Auth.Domain.Entities;
+
+namespace Membera.Auth.Application.Abstractions;
+
+public interface IRefreshTokenRepository
+{
+    Task AddAsync(RefreshToken refreshToken);
+    Task<RefreshToken?> GetByTokenAsync(string token);
+    Task UpdateAsync(RefreshToken refreshToken);
+}
--- membera-backend/src/services/auth/Membera.Auth.Application/obj/Debug/net8.0/Membera.Auth.Application.AssemblyInfo.cs
+++ membera-backend/src/services/auth/Membera.Auth.Application/obj/Debug/net8.0/Membera.Auth.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Membera.Auth.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acc28f7f1bfd0ebba7924ed9fe88c68fb901d899")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a48624d1e2e1929633efd9a843d7564edacc8ac5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Membera.Auth.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Membera.Auth.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
