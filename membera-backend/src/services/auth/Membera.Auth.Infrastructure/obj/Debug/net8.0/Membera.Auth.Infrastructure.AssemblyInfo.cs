e3f8ed605bc76d1d4cc9b10233bc6a01ea959af6
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-09-04 15:50:53 +0400

    RefreshTokenConfiguration

--- membera-backend/src/services/auth/Membera.Auth.Infrastructure/obj/Debug/net8.0/Membera.Auth.Infrastructure.AssemblyInfo.cs
+++ membera-backend/src/services/auth/Membera.Auth.Infrastructure/obj/Debug/net8.0/Membera.Auth.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Membera.Auth.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e4a620de4594dcd446b4bc2ad97b721407e20c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+755ba67de76537ee7cba4b70dca606f92583c12b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Membera.Auth.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Membera.Auth.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
