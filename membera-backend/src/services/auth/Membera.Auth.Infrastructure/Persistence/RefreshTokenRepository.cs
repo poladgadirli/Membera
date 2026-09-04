8b9e39c991ed2e7c43d03e61585737db8323482c
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-09-04 17:31:50 +0400

    RefreshTokenRepository

---
+++ membera-backend/src/services/auth/Membera.Auth.Infrastructure/Persistence/RefreshTokenRepository.cs
@@ -0,0 +1,33 @@
+﻿using Membera.Auth.Application.Abstractions;
+using Membera.Auth.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Membera.Auth.Infrastructure.Persistence;
+
+public class RefreshTokenRepository : IRefreshTokenRepository
+{
+    private readonly AuthDbContext _context;
+
+    public RefreshTokenRepository(AuthDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(RefreshToken refreshToken)
+    {
+        await _context.RefreshTokens.AddAsync(refreshToken);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<RefreshToken?> GetByTokenAsync(string token)
+    {
+        return await _context.RefreshTokens
+            .FirstOrDefaultAsync(rt => rt.Token == token);
+    }
+
+    public async Task UpdateAsync(RefreshToken refreshToken)
+    {
+        _context.RefreshTokens.Update(refreshToken);
+        await _context.SaveChangesAsync();
+    }
+}
