677a80985cdff8cc7726bcde090daf4bdeb95b99
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-09-04 15:55:45 +0400

    UserRepository

---
+++ membera-backend/src/services/auth/Membera.Auth.Infrastructure/Persistence/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using Membera.Auth.Application.Abstractions;
+using Membera.Auth.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Membera.Auth.Infrastructure.Persistence;
+
+public class UserRepository : IUserRepository
+{
+    private readonly AuthDbContext _context;
+
+    public UserRepository(AuthDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        return await _context.Users
+            .FirstOrDefaultAsync(u => u.Email == email);
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+        await _context.SaveChangesAsync();
+    }
+}
