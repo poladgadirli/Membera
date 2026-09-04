674d0196187c42f4aa5d5bd3a972a2a0e3a02391
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-09-04 17:53:21 +0400

    RefreshAccessTokenResult

--- membera-backend/src/services/auth/Membera.Auth.Application/obj/Debug/net8.0/Membera.Auth.Application.AssemblyInfo.cs
+++ membera-backend/src/services/auth/Membera.Auth.Application/obj/Debug/net8.0/Membera.Auth.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Membera.Auth.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80c45293184178cd7a6d52bd6bc59346ed97c132")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3a764f3d11f2fa7172c636602a3b716f013a5e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Membera.Auth.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Membera.Auth.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
