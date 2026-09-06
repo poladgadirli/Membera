9d828b0cfa3b822a48590e240160b998d8ea3d90
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-09-06 13:52:03 +0400

    UserRegisteredEvent

---
+++ membera-backend/src/BuildingBlocks/Membera.Shared.Contracts/UserRegisteredEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Membera.Shared.Contracts;
+
+public record UserRegisteredEvent(
+    Guid UserId,
+    string Email,
+    string FirstName,
+    string LastName,
+    string Role,
+    DateTime RegisteredAt
+);
