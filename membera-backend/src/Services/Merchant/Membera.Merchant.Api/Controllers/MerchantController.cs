b571b886c9b45dc1c4100954a9c73630b6c36bd0
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-09-07 15:57:49 +0400

    Merchant controller

---
+++ membera-backend/src/Services/Merchant/Membera.Merchant.Api/Controllers/MerchantController.cs
@@ -0,0 +1,46 @@
+﻿using Membera.Merchant.Application.Merchants.CreateMerchant;
+using Membera.Merchant.Application.Merchants.GetMerchantByOwnerId;
+using Membera.Merchant.Application.Merchants.UpdateMerchant;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Membera.Merchant.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MerchantController : ControllerBase
+{
+    private readonly CreateMerchantHandler _createMerchantHandler;
+    private readonly GetMerchantByOwnerIdHandler _getMerchantByOwnerIdHandler;
+    private readonly UpdateMerchantHandler _updateMerchantHandler;
+
+    public MerchantController(
+        CreateMerchantHandler createMerchantHandler,
+        GetMerchantByOwnerIdHandler getMerchantByOwnerIdHandler,
+        UpdateMerchantHandler updateMerchantHandler)
+    {
+        _createMerchantHandler = createMerchantHandler;
+        _getMerchantByOwnerIdHandler = getMerchantByOwnerIdHandler;
+        _updateMerchantHandler = updateMerchantHandler;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateMerchantCommand command)
+    {
+        var result = await _createMerchantHandler.HandleAsync(command);
+        return Ok(result);
+    }
+
+    [HttpGet("{ownerId}")]
+    public async Task<IActionResult> GetByOwnerId(Guid ownerId)
+    {
+        var result = await _getMerchantByOwnerIdHandler.HandleAsync(new GetMerchantByOwnerIdQuery(ownerId));
+        return Ok(result);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update(UpdateMerchantCommand command)
+    {
+        await _updateMerchantHandler.HandleAsync(command);
+        return NoContent();
+    }
+}
