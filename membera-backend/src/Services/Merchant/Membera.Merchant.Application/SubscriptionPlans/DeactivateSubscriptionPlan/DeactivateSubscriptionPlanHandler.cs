2dab07df85b05ae7cc78d006444c7d93c601ebfd
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-09-09 10:56:08 +0400

    DeactivateSubscriptionPlanCommand

---
+++ membera-backend/src/Services/Merchant/Membera.Merchant.Application/SubscriptionPlans/DeactivateSubscriptionPlan/DeactivateSubscriptionPlanHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Membera.Merchant.Application.SubscriptionPlans.DeactivateSubscriptionPlan
+{
+    internal class DeactivateSubscriptionPlanHandler
+    {
+    }
+}
