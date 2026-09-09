fb522ff91e9d4c6f0fed725504c6331c73c63cbe
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-09-09 10:51:13 +0400

    UpdateSubscriptionPlanCommand

---
+++ membera-backend/src/Services/Merchant/Membera.Merchant.Application/SubscriptionPlans/UpdateSubscriptionPlan/UpdateSubscriptionPlanCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Membera.Merchant.Application.SubscriptionPlans.UpdateSubscriptionPlan;
+
+public record UpdateSubscriptionPlanCommand(
+    Guid PlanId,
+    Guid MerchantId,
+    string Name,
+    string? Description,
+    decimal Price,
+    int DurationInDays,
+    int? UsageLimit,
+    TimeOnly ActiveFrom,
+    TimeOnly ActiveUntil
+);
