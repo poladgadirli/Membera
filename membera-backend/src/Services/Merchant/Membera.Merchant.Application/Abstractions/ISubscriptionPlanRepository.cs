8b1aa6fc222179d984768dec9aadf70b55751cdf
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-09-09 10:44:30 +0400

    ISubscriptionPlanRepository

---
+++ membera-backend/src/Services/Merchant/Membera.Merchant.Application/Abstractions/ISubscriptionPlanRepository.cs
@@ -0,0 +1,11 @@
+﻿using Membera.Merchant.Domain.Entities;
+
+namespace Membera.Merchant.Application.Abstractions;
+
+public interface ISubscriptionPlanRepository
+{
+    Task<SubscriptionPlan?> GetByIdAsync(Guid id);
+    Task<List<SubscriptionPlan>> GetByMerchantIdAsync(Guid merchantId);
+    Task AddAsync(SubscriptionPlan plan);
+    Task UpdateAsync(SubscriptionPlan plan);
+}
